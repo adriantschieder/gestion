2cb6138643aeeab5e72b1ded31e56609099d860c
Adrian Tschieder <adrian_tschieder@hotmail.com>
2022-01-16 01:52:23 -0300

    Arreglos de las maquinarias

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 from .views import (
-     
+     #funciones de Clientes
+
+     #funciones de Maquinarias
+
+     #funciones de Arreglos
+     arreglos,arreglos_cliente,agregar_arreglos_clientes,arreglos_editados,eliminar_arreglo,
      )
 
 urlpatterns = [
-     
-     
+     path('arreglos/',arreglos, name ='arreglos'),
+     path('arreglos-cliente/<id>', arreglos_cliente, name = 'arreglosclientes'),
+     # path('arreglos-agregar/<idmaquina>', arreglos_agregar, name = 'agregar-arreglos'),
+     path('arreglos-agregados/<int:id>',agregar_arreglos_clientes, name='arreglos-agregados'),
+     # path('ver-arreglos/<id>/<idmaquina>', editar_arreglos_cliente, name = 'ver arreglos'),
+     path('arreglos-editados/<id>/<idmaquina>', arreglos_editados, name='arregloseditados'),
+     path('eliminar-arreglo/<id>/<idmaquina>', eliminar_arreglo, name = 'eliminar-arreglo'),
 
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,67 @@
 from django.shortcuts import redirect, render
 from clientes.models import Clientes, Maquinarias, Arreglo_Maquinarias
-#Funciones para Pestaña de Clientes
+#Funciones para Pestaña de Arreglos
+def arreglos(request):
+    arreglos = Arreglo_Maquinarias.objects.all()
+    maquinarias = Maquinarias.objects.all()
+    return render(request, 'arreglos.html', {"arreglos": arreglos, "maquinarias": maquinarias})
+
+def arreglos_cliente(request,id):
+    maquinarias = Maquinarias.objects.get(id=id)
+    arreglos = Arreglo_Maquinarias.objects.filter(maquinaria=id)
+    return render(request, 'arreglos-cliente.html', {"arreglos": arreglos, "maquinarias": maquinarias})
+
+# def arreglos_agregar(request,idmaquina):
+#     maquinarias = Maquinarias.objects.get(id=idmaquina)
+#     arreglos = Arreglo_Maquinarias.objects.all()
+#     return render(request, 'arreglos-agregar.html', {"arreglos": arreglos, "maquinarias": maquinarias})
+
+def arreglos_agregados(request):
+    maquinarias = Arreglo_Maquinarias.objects.get(pk = request.POST['maquinarias'])
+    fecha_arreglo = request.POST['fecha_arreglo']
+    arreglo1 = request.POST['arreglo']
+    arreglo = Arreglo_Maquinarias.objects.create(
+        maquinarias = maquinarias,
+        fecha_arreglo = fecha_arreglo,
+        arreglo = arreglo1,
+    )
+    return redirect('../arreglos')
+
+# def arreglos_agregar(request,idmaquina):
+#     maquinarias = Maquinarias.objects.get(id=idmaquina)
+#     arreglos = Arreglo_Maquinarias.objects.all
+#     return render(request, 'arreglos-agregar.html', {"arreglos": arreglos, "maquinarias": maquinarias})
+
+def agregar_arreglos_clientes(request,id):
+    maquinaria = Maquinarias.objects.get(pk = request.POST['maquinaria'])
+    fecha_arreglo = request.POST['fecha_arreglo']
+    arreglo1 = request.POST['arreglo']
+    arreglo = Arreglo_Maquinarias.objects.create(
+        maquinaria = maquinaria,
+        fecha_arreglo = fecha_arreglo,
+        arreglo = arreglo1,
+    )
+    return redirect(f'/clientes/arreglos-cliente/{id}')
+
+def eliminar_arreglo(request,id, idmaquina):
+    arreglo = Arreglo_Maquinarias.objects.get(id=id)
+    arreglo.delete()
+    return redirect(f'/clientes/arreglos-cliente/{idmaquina}')
+
+def editar_arreglos_cliente(request,id,idmaquina):
+    maquinarias = Maquinarias.objects.get(id=idmaquina)
+    arreglos = Arreglo_Maquinarias.objects.get(id=id)
+    return render(request, 'ver-arreglos.html', {"arreglos": arreglos, "maquinarias": maquinarias})
+
+def arreglos_editados(request,id,idmaquina):
+    maquinaria = Maquinarias.objects.get(pk = request.POST['maquinaria'])
+    fecha_arreglo = request.POST['fecha_arreglo']
+    arreglo1 = request.POST['arreglo']
+    arreglo = Arreglo_Maquinarias.objects.get(id=id)
+    arreglo.fecha_arreglo = fecha_arreglo
+    arreglo.arreglo = arreglo1
+    arreglo.save()
+    return redirect(f'/clientes/arreglos-cliente/{idmaquina}')
+
+
+#FIN Funciones para Pestaña de Arreglos
